5d7cd2766d4a10589e6a4b50469662e0cf05167e
Nikita <nikitakorzh10@gmail.com>
2021-09-28 18:09:04 +0300

    Upgrade Lab1

--- lab1/src/main/java/ua/kpi/App.java
+++ lab1/src/main/java/ua/kpi/App.java
@@ -1,8 +1,20 @@
 package ua.kpi;
 
+import java.util.Arrays;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        Possibles a = new Possibles();
+        Variant b =new Variant();
+        //System.out.println(a.toString());
+        //System.out.println(Arrays.toString(a.P_M(b.M, b.k, b.C)));
+        //int[][] g ={{3,2},{4,5}};
+        //System.out.println(b.C.length);
+        double[][] c = a.P_M_CC(b.M, b.k, b.C);
+        for (int i = 0; i < c.length ; i++) {
+            System.out.println(Arrays.toString(c[i]));
+        }
     }
 }
