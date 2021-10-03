9862d1fc68b8317e0cfd6cb50cfd14d2cf864f6f
tanastasiia <anastasia.taftay@gmail.com>
2021-10-03 15:49:21 +0300

    small refactoring

--- lab1/src/main/java/ua/kpi/Possibles.java
+++ lab1/src/main/java/ua/kpi/Possibles.java
@@ -3,7 +3,7 @@ package ua.kpi;
 public class Possibles extends Variant {
 
     public double[] prC(double[] M, double[] k, int[][] C) {
-        double[] prC = new double[20];
+        double[] prC = new double[C.length];
         for (int j = 0; j < C.length; j++) {
             for (int l = 0; l < C.length; l++) {
                 prC[C[j][l]] = prC[C[j][l]] + k[j] * M[l];
@@ -14,7 +14,7 @@ public class Possibles extends Variant {
 
     //P(C,M)
     public double[][] prMC(double[] M, double[] k, int[][] C) {
-        double[][] prMC = new double[20][20];
+        double[][] prMC = new double[C.length][C.length];
         for (int j = 0; j < C.length; j++) { // rows - k
             for (int l = 0; l < C.length; l++) { //columns - M
                 prMC[C[j][l]][l] = prMC[C[j][l]][l] + k[j] * M[l];
@@ -25,7 +25,7 @@ public class Possibles extends Variant {
 
     //P(M|C) = P(M,C) / P(C)
     public double[][] prMCConditional(double[] M, double[] k, int[][] C) {
-        double[][] res = new double[20][20];
+        double[][] res = new double[C.length][C.length];
         double[] prC = prC(M, k, C);
         double[][] prMC = prMC(M, k, C);
 
